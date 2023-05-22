91f88bd284525c6f0f988894ccf49d0aa62bdd90
CarlyCa <100634408+CarlyCa@users.noreply.github.com>
2023-05-22 12:51:57 -0400

    Create hr_style.js

---
+++ hr_style.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>API Test</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+  </head>
+  <body>
+    <form onsubmit="submitForm(event)">
+      <input type="text" id="input" placeholder="Ask a stats question" />
+      <button type="submit">Submit</button>
+    </form>
+    <div id="result"></div>
+    <div id="log"></div>
+    <script src="hr_script.js"></script>
+  </body>
+</html>
